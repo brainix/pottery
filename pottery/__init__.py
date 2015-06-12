b17cdd98030122d5e12131e0432cafbc3fcc2963
Raj Shah <brainix@gmail.com>
2015-06-12 02:39:45 -0700

    bumped version number

--- pottery/__init__.py
+++ pottery/__init__.py
@@ -6,7 +6,7 @@
 #-----------------------------------------------------------------------------#
 
 __title__ = 'pottery'
-__version__ = '0.2'
+__version__ = '0.3'
 __author__ = 'Rajiv Bakulesh Shah'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright (c) 2015, Rajiv Bakulesh Shah'
