e5513a0e31cb674e4a9f62a107bec15465d59e4f
Raj Shah <brainix@gmail.com>
2015-06-13 06:43:58 -0700

    documented computational complexity, improved performance of RedisSet.__init__(), RedisSet.__repr__(), and RedisDict.__repr__()

--- pottery/containers.py
+++ pottery/containers.py
@@ -34,54 +34,78 @@ class _Base:
 
 class RedisSet(_Base, collections.abc.MutableSet):
     def __init__(self, redis, key, iterable=tuple()):
+        """Initialize a RedisSet.  O(n)"""
         super().__init__(redis, key, iterable)
-        for value in iterable:
-            self._redis.sadd(self._key, json.dumps(value))
+        values = [json.dumps(value) for value in iterable]
+        self._redis.sadd(self._key, *values)
 
     def __contains__(self, value):
+        """s.__contains__(element) <==> element in s.  O(1)"""
         return self._redis.sismember(self._key, json.dumps(value))
 
     def __iter__(self):
+        """Iterate over the elements in a RedisSet.  O(n)"""
         return super().__iter__(self._redis.sscan)
 
     def __len__(self):
+        """Return the number of elements in a RedisSet.  O(1)"""
         return self._redis.scard(self._key)
 
     def add(self, value):
+        """Add an element to a RedisSet.  O(1)
+
+        This has no effect if the element is already present.
+        """
         self._redis.sadd(self._key, json.dumps(value))
 
     def discard(self, value):
+        """Remove an element from a RedisSet.  O(1)
+
+        This has no effect if the element is not present.
+        """
         self._redis.srem(self._key, json.dumps(value))
 
     def __repr__(self):
-        return self.__class__.__name__ + str(set(self))
+        """Return the string representation of a RedisSet.  O(n)"""
+        s = self._redis.smembers(self._key)
+        s = set(json.loads(value.decode('utf-8')) for value in s)
+        return self.__class__.__name__ + str(s)
 
 
 
 class RedisDict(_Base, collections.abc.MutableMapping):
     def __init__(self, redis, key, **kwargs):
+        """Initialize a RedisDict.  O(n)"""
         super().__init__(redis, key, **kwargs)
         self.update(**kwargs)
 
     def __getitem__(self, key):
+        """d.__getitem__(key) <==> d[key].  O(1)"""
         value = self._redis.hget(self._key, key)
         if value is None:
             raise KeyError(key)
         return json.loads(value.decode('utf-8'))
 
     def __setitem__(self, key, value):
+        """d.__setitem__(key, value) <==> d[key] = value.  O(1)"""
         self._redis.hset(self._key, key, json.dumps(value))
 
     def __delitem__(self, key):
+        """d.__delitem__(key) <==> del d[key].  O(1)"""
         success = self._redis.hdel(self._key, key)
         if not bool(success):
             raise KeyError(key)
 
     def __iter__(self):
+        """Iterate over the items in a RedisDict.  O(n)"""
         return super().__iter__(self._redis.hscan)
 
     def __len__(self):
+        """Return the number of items in a RedisDict.  O(1)"""
         return self._redis.hlen(self._key)
 
     def __repr__(self):
-        return self.__class__.__name__ + str(dict(self))
+        """Return the string representation of a RedisDict."""
+        d = self._redis.hgetall(self._key).items()
+        d = {k.decode('utf-8'): json.loads(v.decode('utf-8')) for k, v in d}
+        return self.__class__.__name__ + str(d)
