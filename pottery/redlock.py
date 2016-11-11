bd71d579a4b76d01c47f218c383fe939903d5fd6
Raj Shah <rajiv@styleseat.com>
2016-11-11 14:55:51 -0800

    Author Redlock doctests

--- pottery/redlock.py
+++ pottery/redlock.py
@@ -5,6 +5,10 @@
 #   All rights reserved.                                                      #
 #-----------------------------------------------------------------------------#
 '''Distributed Redis-powered lock.
+    
+This algorithm safely and reliably provides a mutually-exclusive locking
+primitive to protect a resource shared across threads, processes, and even
+machines, without a single point of failure.
 
 Rationale and algorithm description:
     http://redis.io/topics/distlock
@@ -34,7 +38,75 @@ from .contexttimer import contexttimer
 
 
 class Redlock(Primitive):
-    'Distributed Redis-powered lock.'
+    '''Distributed Redis-powered lock.
+    
+    This algorithm safely and reliably provides a mutually-exclusive locking
+    primitive to protect a resource shared across threads, processes, and even
+    machines, without a single point of failure.
+
+    Rationale and algorithm description:
+        http://redis.io/topics/distlock
+
+    Usage:
+
+        >>> printer_lock = Redlock(key='printer')
+        >>> bool(printer_lock.locked())
+        False
+        >>> printer_lock.acquire()
+        True
+        >>> bool(printer_lock.locked())
+        True
+        >>> # Critical section - print stuff here.
+        >>> printer_lock.release()
+        >>> bool(printer_lock.locked())
+        False
+
+    Redlocks time out (by default, after 10 seconds).  You should take care to
+    ensure that your critical section completes well within the timeout.  The
+    reasons that Redlocks time out are to preserve "liveness"
+    (http://redis.io/topics/distlock#liveness-arguments) and to avoid deadlocks
+    (in the event that a process dies inside a critical section before it
+    releases its lock).
+
+        >>> printer_lock.acquire()
+        True
+        >>> bool(printer_lock.locked())
+        True
+        >>> # Critical section - print stuff here.
+        >>> time.sleep(10)
+        >>> bool(printer_lock.locked())
+        False
+
+    If 10 seconds isn't enough to complete executing your critical section,
+    then you can specify your own timeout:
+
+        >>> printer_lock = Redlock(key='printer', auto_release_time=15*1000)
+        >>> printer_lock.acquire()
+        True
+        >>> bool(printer_lock.locked())
+        True
+        >>> # Critical section - print stuff here.
+        >>> time.sleep(15)
+        >>> bool(printer_lock.locked())
+        False
+
+    You can use a Redlock as a context manager:
+
+        >>> states = []
+        >>> with Redlock(key='printer') as printer_lock:
+        ...     states.append(bool(printer_lock.locked()))
+        ...     # Critical section - print stuff here.
+        >>> states.append(bool(printer_lock.locked()))
+        >>> states
+        [True, False]
+
+        >>> states = []
+        >>> with printer_lock:
+        ...     states.append(bool(printer_lock.locked()))
+        >>> states.append(bool(printer_lock.locked()))
+        >>> states
+        [True, False]
+    '''
 
     KEY_PREFIX = 'redlock'
     AUTO_RELEASE_TIME = 10 * 1000
@@ -149,6 +221,49 @@ class Redlock(Primitive):
             return False
 
     def acquire(self, *, blocking=True, timeout=-1):
+        '''Lock the lock.
+
+        If blocking is True and timeout is -1, then wait for as long as
+        necessary to acquire the lock.  Return True.
+
+            >>> printer_lock_1 = Redlock(key='printer')
+            >>> printer_lock_1.acquire()
+            True
+            >>> timer = contexttimer()
+            >>> timer.start()
+            >>> printer_lock_2 = Redlock(key='printer')
+            >>> printer_lock_2.acquire()
+            True
+            >>> 10 * 1000 < timer.elapsed < 11 * 1000
+            True
+            >>> printer_lock_2.release()
+
+        If blocking is True and timeout is not -1, then wait for up to timeout
+        seconds to acquire the lock.  Return True if the lock was acquired;
+        False if it wasn't.
+
+            >>> printer_lock_1.acquire()
+            True
+            >>> printer_lock_2.acquire(timeout=15)
+            True
+            >>> printer_lock_2.release()
+
+            >>> printer_lock_1.acquire()
+            True
+            >>> printer_lock_2.acquire(timeout=1)
+            False
+            >>> printer_lock_1.release()
+
+        If blocking is False and timeout is -1, then try just once right now to
+        acquire the lock.  Return True if the lock was acquired; False if it
+        wasn't.
+
+            >>> printer_lock_1.acquire()
+            True
+            >>> printer_lock_2.acquire(blocking=False)
+            False
+            >>> printer_lock_1.release()
+        '''
         if blocking:
             with contexttimer() as timer:
                 while timeout == -1 or timer.elapsed / 1000 < timeout:
@@ -163,6 +278,31 @@ class Redlock(Primitive):
             raise ValueError("can't specify a timeout for a non-blocking call")
 
     def locked(self):
+        '''How much longer we'll hold the lock (unless we extend or release it).
+
+        If we don't currently hold the lock, then this method returns 0.
+
+            >>> printer_lock_1 = Redlock(key='printer')
+            >>> printer_lock_1.locked()
+            0
+
+            >>> printer_lock_2 = Redlock(key='printer')
+            >>> printer_lock_2.acquire()
+            True
+            >>> printer_lock_1.locked()
+            0
+            >>> printer_lock_2.release()
+
+        If we do currently hold the lock, then this method returns the current
+        lease's Time To Live (TTL) in ms.
+
+            >>> printer_lock_1.acquire()
+            True
+            >>> 9 * 1000 < printer_lock_1.locked() < 10 * 1000
+            True
+            >>> printer_lock_1.release()
+
+        '''
         with contexttimer() as timer, \
              concurrent.futures.ThreadPoolExecutor(max_workers=len(self.masters)) as executor:
             num_masters_acquired, ttls = 0, []
@@ -183,6 +323,24 @@ class Redlock(Primitive):
                 return 0
 
     def extend(self):
+        '''Extend our hold on the lock (if we currently hold it).
+
+        Usage:
+
+            >>> printer_lock = Redlock(key='printer')
+            >>> printer_lock.acquire()
+            True
+            >>> 9 * 1000 < printer_lock.locked() < 10 * 1000
+            True
+            >>> time.sleep(1)
+            >>> 8 * 1000 < printer_lock.locked() < 9 * 1000
+            True
+            >>> printer_lock.extend()
+            True
+            >>> 9 * 1000 < printer_lock.locked() < 10 * 1000
+            True
+            >>> printer_lock.release()
+        '''
         if self._extension_num >= self.num_extensions:
             raise RuntimeError('extend lock too many times')
         else:
@@ -198,6 +356,21 @@ class Redlock(Primitive):
             return quorum
 
     def release(self):
+        '''Unlock the lock.
+
+        Usage:
+
+            >>> printer_lock = Redlock(key='printer')
+            >>> bool(printer_lock.locked())
+            False
+            >>> printer_lock.acquire()
+            True
+            >>> bool(printer_lock.locked())
+            True
+            >>> printer_lock.release()
+            >>> bool(printer_lock.locked())
+            False
+        '''
         num_masters_released = 0
         with concurrent.futures.ThreadPoolExecutor(max_workers=len(self.masters)) as executor:
             futures = {executor.submit(self._release_master, master)
@@ -210,9 +383,48 @@ class Redlock(Primitive):
             raise RuntimeError('release unlocked lock')
 
     def __enter__(self):
-        return self.acquire()
+        '''You can use a Redlock as a context manager.
+
+        Usage:
+
+            >>> states = []
+            >>> with Redlock(key='printer') as printer_lock:
+            ...     states.append(bool(printer_lock.locked()))
+            ...     # Critical section - print stuff here.
+            >>> states.append(bool(printer_lock.locked()))
+            >>> states
+            [True, False]
+
+            >>> states = []
+            >>> with printer_lock:
+            ...     states.append(bool(printer_lock.locked()))
+            >>> states.append(bool(printer_lock.locked()))
+            >>> states
+            [True, False]
+        '''
+        self.acquire()
+        return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        '''You can use a Redlock as a context manager.
+
+        Usage:
+
+            >>> states = []
+            >>> with Redlock(key='printer') as printer_lock:
+            ...     states.append(bool(printer_lock.locked()))
+            ...     # Critical section - print stuff here.
+            >>> states.append(bool(printer_lock.locked()))
+            >>> states
+            [True, False]
+
+            >>> states = []
+            >>> with printer_lock:
+            ...     states.append(bool(printer_lock.locked()))
+            >>> states.append(bool(printer_lock.locked()))
+            >>> states
+            [True, False]
+        '''
         self.release()
 
     def __repr__(self):
