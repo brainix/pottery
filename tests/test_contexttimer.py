ca38bf50f71b6e09e12346e82c73299d10d36d79
Raj Shah <brainix@gmail.com>
2016-11-13 04:46:18 -0800

    Make contexttimer more usable...
    …standalone, not just as a context manager.

--- tests/test_contexttimer.py
+++ tests/test_contexttimer.py
@@ -19,16 +19,47 @@ from tests.base import TestCase
 class ContextTimerTests(TestCase):
     ACCURACY = 50   # in milliseconds
 
-    def _confirm_elapsed(self, timer, expected):
-        got = round(timer.elapsed / self.ACCURACY) * self.ACCURACY
+    def setUp(self):
+        super().setUp()
+        self.timer = contexttimer()
+
+    def _confirm_elapsed(self, expected):
+        got = round(self.timer.elapsed / self.ACCURACY) * self.ACCURACY
         assert got == expected, '{} != {}'.format(got, expected)
 
-    def test_contexttimer(self):
-        with contexttimer() as timer:
-            self._confirm_elapsed(timer, 0)
+    def test_start_stop_and_elapsed(self):
+        # timer hasn't been started
+        with self.assertRaises(RuntimeError):
+            self.timer.elapsed
+        with self.assertRaises(RuntimeError):
+            self.timer.stop()
+
+        # timer has been started but not stopped
+        self.timer.start()
+        with self.assertRaises(RuntimeError):
+            self.timer.start()
+        time.sleep(0.1)
+        self._confirm_elapsed(1*100)
+        self.timer.stop()
+
+        # timer has been stopped
+        with self.assertRaises(RuntimeError):
+            self.timer.start()
+        time.sleep(0.1)
+        self._confirm_elapsed(1*100)
+        with self.assertRaises(RuntimeError):
+            self.timer.stop()
+
+    def test_context_manager(self):
+        with self.timer as timer:
+            self._confirm_elapsed(0)
             for iteration in range(1, 3):
                 with self.subTest(iteration=iteration):
                     time.sleep(0.1)
-                    self._confirm_elapsed(timer, iteration*100)
-            self._confirm_elapsed(timer, iteration*100)
-        self._confirm_elapsed(timer, iteration*100)
+                    self._confirm_elapsed(iteration*100)
+            self._confirm_elapsed(iteration*100)
+        time.sleep(0.1)
+        self._confirm_elapsed(iteration*100)
+
+        with self.assertRaises(RuntimeError), self.timer as timer:
+            ...
