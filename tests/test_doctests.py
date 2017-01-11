a1acf2b38b63e012da4aa8aa161762b3d899cd92
Rajiv Bakulesh Shah <brainix@gmail.com>
2017-01-10 19:51:26 -0800

    Run (slow) doctests on only CI
    Make it faster/easier/cheaper to run through the entire unit test suite
    while writing/debugging code locally.

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -10,6 +10,7 @@
 import doctest
 import importlib
 import os
+import unittest
 
 from tests.base import TestCase
 
@@ -27,6 +28,7 @@ class DoctestTests(TestCase):
             module = importlib.import_module('pottery.{}'.format(module_name))
             yield module
 
+    @unittest.skipUnless('CI' in os.environ, 'run (slow) doctests on only CI')
     def test_doctests(self):
         for module in self._modules:
             with self.subTest(module=module):
