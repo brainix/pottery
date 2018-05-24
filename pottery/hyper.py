df0448e0e9155888097e6bb2b76cd91fd2ae5f2b
Rajiv Bakulesh Shah <brainix@gmail.com>
2018-05-24 16:45:44 -0700

    Design/implement/test HyperLogLog (#81)
    * Design/implement/test HyperLogLog
    
    antirez's HyperLogLog blog post:
    http://antirez.com/news/75
    
    * Implement/test HyperLogLog.__repr__()
    
    * Upgrade requirements
    
    * Rename contexttimer to timer
    
    This keeps our names nice and parallel:
    
    ```
    from .bloom import BloomFilter
    from .cache import redis_cache
    from .hyper import HyperLogLog
    from .nextid import NextId
    from .redlock import Redlock
    from .timer import ContextTimer
    ```
    
    * Prefer kwargs to positional args
    
    Explicit is better than implicit.  `__doc__.split(sep='\n\n',
    maxsplit=1)` reads better than `__doc__.split('\n\n', 1)`.
    
    * Upgrade Python to 3.6.5

---
+++ pottery/hyper.py
@@ -0,0 +1,69 @@
+#-----------------------------------------------------------------------------#
+#   hyper.py                                                                  #
+#                                                                             #
+#   Copyright © 2015-2018, Rajiv Bakulesh Shah, original author.              #
+#   All rights reserved.                                                      #
+#-----------------------------------------------------------------------------#
+
+
+
+from .base import Base
+
+
+
+class HyperLogLog(Base):
+    '''Redis-backed HyperLogLog with a Pythonic API.
+
+    Wikipedia article:
+        https://en.wikipedia.org/wiki/HyperLogLog
+
+    antirez's blog post:
+        http://antirez.com/news/75
+    '''
+
+    def __init__(self, iterable=frozenset(), *, redis=None, key=None):
+        '''Initialize a HyperLogLog.  O(n)
+
+        Here, n is the number of elements in iterable that you want to insert
+        into this HyperLogLog.
+        '''
+        super().__init__(redis=redis, key=key)
+        self.update(iterable)
+
+    def add(self, value):
+        'Add an element to a HyperLogLog.  O(1)'
+        self.update({value})
+
+    def update(self, *objs):
+        objs, other_hll_keys, encoded_values = (self,) + tuple(objs), [], []
+        for obj in objs:
+            if isinstance(obj, self.__class__):
+                other_hll_keys.append(obj.key)
+            else:
+                encoded_values.extend(self._encode(value) for value in obj)
+        with self._watch(objs[1:]):
+            self.redis.multi()
+            self.redis.pfmerge(self.key, *other_hll_keys)
+            self.redis.pfadd(self.key, *encoded_values)
+
+    def union(self, *objs, redis=None, key=None):
+        new_hll = self.__class__(redis=redis, key=key)
+        new_hll.update(self, *objs)
+        return new_hll
+
+    def __len__(self):
+        '''Return the approximate number of elements in a HyperLogLog.  O(1)
+
+        Please note that this method returns an approximation, not an exact
+        value.  So please don't rely on it for anything important like
+        financial systems or cat gif websites.
+        '''
+        return self.redis.pfcount(self.key)
+
+    def __repr__(self):
+        'Return the string representation of a HyperLogLog.  O(1)'
+        return '<{} key={} len={}>'.format(
+            self.__class__.__name__,
+            self.key,
+            len(self),
+        )
