b00e64b11628d325535c85e670977cc717d66142
Rajiv Bakulesh Shah <brainix@gmail.com>
2019-01-03 16:01:04 -0800

    Happy new year! (#100)

--- tests/test_hyper.py
+++ tests/test_hyper.py
@@ -1,7 +1,7 @@
 #-----------------------------------------------------------------------------#
 #   test_hyper.py                                                             #
 #                                                                             #
-#   Copyright © 2015-2018, Rajiv Bakulesh Shah, original author.              #
+#   Copyright © 2015-2019, Rajiv Bakulesh Shah, original author.              #
 #   All rights reserved.                                                      #
 #-----------------------------------------------------------------------------#
 
