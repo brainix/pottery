42ad1bb08fceae597532b128ed1dc9ed02ee7e5f
Raj Shah <rajiv.shah@reddit.com>
2019-02-25 16:56:16 -0800

    Bump version number

--- pottery/__init__.py
+++ pottery/__init__.py
@@ -14,7 +14,7 @@ know how to use Pottery.
 
 
 __title__ = 'pottery'
-__version__ = '0.57'
+__version__ = '0.58'
 __description__, __long_description__ = (
     s.strip() for s in __doc__.split(sep='\n\n', maxsplit=1)
 )
