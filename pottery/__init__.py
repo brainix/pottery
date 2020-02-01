8c0a5b0fbb7f8afb57083613f6d08ec2783d469f
Rajiv Bakulesh Shah <brainix@gmail.com>
2020-02-01 00:34:24 -0800

    Revert "Oops; make Pottery require redis-py >= 3.4.0 (#167)"
    This reverts commit 95c01f29e7c4b4e4a62f1834e98bf93afe3b39ae.

--- pottery/__init__.py
+++ pottery/__init__.py
@@ -13,7 +13,7 @@ know how to use Pottery.
 
 
 __title__ = 'pottery'
-__version__ = '0.68'
+__version__ = '0.67'
 __description__, __long_description__ = (
     s.strip() for s in __doc__.split(sep='\n\n', maxsplit=1)
 )
