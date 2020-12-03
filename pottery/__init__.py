9fd1f94fa8eecfff21f11a8e336c296431fc04c2
Rajiv Bakulesh Shah <brainix@gmail.com>
2020-12-02 17:38:36 -0800

    Bump version number (#282)

--- pottery/__init__.py
+++ pottery/__init__.py
@@ -13,7 +13,7 @@ know how to use Pottery.
 
 
 __title__ = 'pottery'
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __description__, __long_description__ = (
     s.strip() for s in __doc__.split(sep='\n\n', maxsplit=1)
 )
