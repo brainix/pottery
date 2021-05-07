1c3febdec7a437bf3386f2002b0e71cda98a56cb
Raj Shah <brainix@gmail.com>
2021-05-07 02:08:46 -0700

    Bump version number

--- pottery/__init__.py
+++ pottery/__init__.py
@@ -30,7 +30,7 @@ from typing_extensions import Final
 
 
 __title__ = 'pottery'
-__version__ = '1.1.7'
+__version__ = '1.1.8'
 __description__, __long_description__ = (
     s.strip() for s in __doc__.split(sep='\n\n', maxsplit=1)
 )
