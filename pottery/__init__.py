0aa47511767de2e88539aafb6ee5c63af91859b3
Rajiv Bakulesh Shah <brainix@gmail.com>
2021-09-29 17:17:29 -0700

    In NextId.reset(), reset all Redis masters (#463)
    * In NextId.reset(), reset all Redis masters
    
    Previously, I'd modified the `NextId.__current_id` getter to get the ID
    from `n // 2 + 1` Redis masters, then to return the highest of those
    IDs.
    
    This means that `NextId.reset()` must delete the Redis key from all
    masters.
    
    * Upgrade requirements

--- pottery/__init__.py
+++ pottery/__init__.py
@@ -30,7 +30,7 @@ from typing_extensions import Final
 
 
 __title__ = 'pottery'
-__version__ = '1.4.1'
+__version__ = '1.4.2'
 __description__, __long_description__ = (
     s.strip() for s in __doc__.split(sep='\n\n', maxsplit=1)
 )
