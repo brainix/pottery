89390c6c9ebe7cb4a82b709d54f5d6376a442bc5
Rajiv Bakulesh Shah <brainix@gmail.com>
2021-12-21 17:10:55 -0800

    Fix linting

--- pottery/base.py
+++ pottery/base.py
@@ -40,7 +40,6 @@ from typing import cast
 from redis import Redis
 from redis import RedisError
 from redis.client import Pipeline
-
 # TODO: When we drop support for Python 3.7, change the following import to:
 #   from typing import Final
 from typing_extensions import Final
