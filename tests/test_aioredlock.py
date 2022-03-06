7ef26a9ae2210060d48b6b7b7acc976d6bf28769
Rajiv Bakulesh Shah <brainix@gmail.com>
2022-03-05 23:12:44 -0800

    Write additional AIORedlock unit tests

--- tests/test_aioredlock.py
+++ tests/test_aioredlock.py
@@ -144,6 +144,22 @@ class AIORedlockTests(TestCase):
             async with self.aioredlock:
                 await self.aioredlock.release()
 
+    def test_context_manager_nonblocking_with_timeout(self):
+        with self.assertRaises(ValueError):
+            AIORedlock(
+                masters={self.aioredis},
+                key='printer',
+                auto_release_time=.2,
+                context_manager_blocking=False,
+                context_manager_timeout=.1
+            )
+
+    @async_test
+    async def test_acquire_nonblocking_with_timeout(self):
+        self._setup()
+        with self.assertRaises(ValueError):
+            await self.aioredlock.acquire(blocking=False, timeout=.1)
+
     @async_test
     async def test_acquire_rediserror(self):
         self._setup()
