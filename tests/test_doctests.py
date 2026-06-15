c96f903eb2527713ec2a8dcde56bb066e0d00da4
Rajiv Bakulesh Shah <brainix@gmail.com>
2026-06-15 08:24:28 -0700

    Improve variable name (#830)

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -31,15 +31,16 @@ def modules() -> list[ModuleType]:
     package_dir = tests_dir.parent
     source_dir = package_dir / 'pottery'
     source_files = source_dir.glob('**/*.py')
-    result = []
+
+    module_list = []
     for source_file in source_files:
         relative_path = source_file.relative_to(package_dir)
         parts = list(relative_path.parts)
         parts[-1] = source_file.stem
         module_name = '.'.join(parts)
         module = importlib.import_module(module_name)
-        result.append(module)
-    return result
+        module_list.append(module)
+    return module_list
 
 
 @pytest.mark.parametrize('module', modules())
